d5157668b0bb5fabe3abbe57d01cbe238cc63246
Rob Earhart <rob@vertex.ai>
2018-05-09 17:37:48 -0700

    Exclude unimplemented functionality

--- tests/test_onnx_backend.py
+++ tests/test_onnx_backend.py
@@ -20,14 +20,32 @@ class BackendTest(onnx.backend.test.BackendTest):
         super(BackendTest, self).__init__(backend, name)
 
         # Unimplemented functionality
+        self.exclude('test_ConvTranspose2d_')  # Requires ConvTranspose
         self.exclude('test_ReflectionPad2d_')  # Requires Pad(reflect)
         self.exclude('test_ReplicationPad2d_')  # Requires Pad(edge)
+        self.exclude('test_averagepool_2d_precomputed_same_upper_')  # Requires Pad(same_upper)
+        self.exclude('test_averagepool_2d_same_lower_')  # Requires Pad(same_lower)
+        self.exclude('test_averagepool_2d_same_upper_')  # Requires Pad(same_upper)
+        self.exclude('test_depthtospace_')  # Requires DepthToSpace
         self.exclude('test_edge_pad_')  # Requires Pad(edge)
-        self.exclude('test_reflect_pad_')  # Requires Pad(reflect)
+        self.exclude('test_maxpool_2d_precomputed_same_lower_')  # Requires Pad(same_lower)
+        self.exclude('test_maxpool_2d_precomputed_same_upper_')  # Requires Pad(same_upper)
+        self.exclude('test_maxpool_2d_same_lower_')  # Requires Pad(same_lower)
+        self.exclude('test_maxpool_2d_same_upper_')  # Requires Pad(same_upper)
         self.exclude('test_gather_1_')  # Requires Gather on non-outermost axis
         self.exclude('test_hardmax_one_hot_')  # Requires filtered Hardmax
+        self.exclude('test_reflect_pad_')  # Requires Pad(reflect)
         self.exclude('test_top_k_')  # Requires TopK
+        self.exclude('test_Upsample_nearest_2d_')  # Requires Upsample
         self.exclude('test_Upsample_nearest_scale_2d_')  # Requires Upsample
+        self.exclude('test_Upsample_nearest_tuple_2d_')  # Requires Upsample
+        self.exclude('test_operator_addmm_')  # Requires Gemm with multiplier broadcast
+        self.exclude('test_operator_convtranspose_')  # Requires ConvTranspose
+        self.exclude('test_operator_lstm_')  # Requires ConstantFill
+        self.exclude('test_operator_rnn_')  # Requires ConstantFill
+        self.exclude('test_operator_reduced_mean_')  # Requires ReduceMean
+        self.exclude('test_operator_reduced_sum_')  # Requires ReduceSum
+        self.exclude('test_operator_pad_')  # Requires additional padding modes
 
     def _add_test(self, category, test_name, test_func, report_item, devices=None):
         if not devices:
